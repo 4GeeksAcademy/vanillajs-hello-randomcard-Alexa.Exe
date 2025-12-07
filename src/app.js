44133d52ba3efe286cc504a2597bf5e908558b14
Alex <alexa.exebio@gmail.com>
2025-12-07 22:25:01 +0000

    color de cartas y picas listo, falta efecto especial As

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"]
+  const numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  
+  const suitTop = document.getElementById("suitTop")
+  const suitBottom = document.getElementById("suitBottom")
+  const numberCard = document.getElementById("numberCard")
+  const randomCardButton = document.getElementById("getRandomCard")
+
+  function randomCard() {
+    suitBottom.classList.remove("text-danger", "text-black")
+    suitTop.classList.remove("text-danger","text-black")
+    numberCard.classList.remove("text-danger", "text-black")
+    
+
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)]
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+    const color = randomSuit == "&hearts;" || randomSuit == "&diams;" ? "text-danger" : "text-black";
+    
+   
+    suitTop.innerHTML = randomSuit
+    suitBottom.innerHTML = randomSuit
+    suitTop.classList.add(color)
+    suitBottom.classList.add(color)
+    numberCard.innerHTML = randomNumber
+    numberCard.classList.add(color)
+  }
+
+  randomCardButton.addEventListener("click", () => randomCard())
+  
+  randomCard()
 };
